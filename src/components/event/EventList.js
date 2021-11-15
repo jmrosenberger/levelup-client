50d0e079b7048c2ee53d11515a9f066b99bed2e6
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-11-15 09:07:26 -0600

    completed chapters 10 and 11, browser does not auto re-render when LEAVE button is clicked

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,51 +1,14 @@
-// import React, { useEffect, useState } from "react"
-// import { useHistory } from "react-router-dom"
-// import { getEvents } from "./EventManager.js"
-
-// export const EventList = (props) => {
-//     const history = useHistory()
-//     const [events, setEvents] = useState([])
-
-//     useEffect(() => {
-//         getEvents().then(data => setEvents(data))
-//     }, [])
-
-//     return (
-//         <article className="events">
-//             <h2>Events List</h2>
-//             <button className="create__event btn-2"
-//                 onClick={() => {
-//                     history.push({ pathname: "/events/new" })
-//                 }}>
-//                 Schedule New Event
-//             </button>
-//             {
-//                 events.map(event => {
-//                     return <section key={`event--${event.id}`} className="event">
-//                         <div className="event__title">{event?.game?.title}</div>
-//                         <div className="event__organizer">Organized By: {event?.organizer?.user.first_name} {event?.organizer?.user.last_name}</div>
-//                         <div className="event__description">Event Description: {event?.description}</div>
-//                         <div className="event__attendees"> Attendees: {event?.attendees}</div>
-//                         <div className="event__dateTime">{event?.date} - {event?.time}</div>
-//                     </section>
-//                 })
-//             }
-//         </article>
-//     )
-// }
-
 import React, { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom"
-import { getEvents, joinEvent } from "./EventManager.js"
+import { getEvents, joinEvent, leaveEvent } from "./EventManager.js"
 // import "./Events.css"
 
 export const EventList = () => {
     const history = useHistory()
-    const [ events, assignEvents ] = useState([])
+    const [ events, updateEvents ] = useState([])
 
     const eventFetcher = () => {
-        getEvents()
-            .then(data => assignEvents(data))
+        getEvents().then(data => updateEvents(data))
     }
 
     useEffect(() => {
@@ -66,22 +29,22 @@ export const EventList = () => {
                 events.map(event => {
                     return <section key={event.id} className="registration">
                         <div className="registration__game">{event.game.title}</div>
-                        <div className="registration__organizer">Organized By: {event?.organizer?.user.first_name} {event?.organizer?.user.last_name}</div>
-                        <div>Event Description: {event.description}</div>
+                        <div>{event.description}</div>
                         <div>
                             {event.date} @ {event.time}
                         </div>
-                        <button className="btn btn-2"
-                                onClick={
-                                    () => {
-                                        joinEvent(event.id, event.joined)
-                                            .then(() => eventFetcher())
-                                    }
-                                }
-                        >{event.joined ? "Leave" : "Join"}</button>
+                        {
+                            event.joined
+                                ? <button className="btn btn-3"
+                                    onClick={() => leaveEvent(event.id).then(() => eventFetcher())}
+                                    >Leave</button>
+                                : <button className="btn btn-2"
+                                    onClick={() => joinEvent(event.id).then(() => eventFetcher())}
+                                    >Join</button>
+                        }
                     </section>
                 })
             }
-        </article >
+        </article>
     )
 }
